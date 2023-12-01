a2aac5a6fdb6c1acb61d46744e77a3e7b685411e
Natalia Braz <naabraz@gmail.com>
2023-12-01 10:02:01 -0300

    chore: add text on view

--- app/src/main/java/br/com/nataliabraz/orgs/MainActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/MainActivity.kt
@@ -2,11 +2,13 @@ package br.com.nataliabraz.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vinda(o) ao Orgs!", Toast.LENGTH_SHORT).show()
+        val view = TextView(this)
+        view.setText("Cesta de Frutas")
+        setContentView(view)
     }
 }
