933865ab9a139707c8f6d2217d423606485fdd73
Natalia Braz <naabraz@gmail.com>
2023-12-05 09:24:33 -0300

    chore: bind code with layout

--- app/src/main/java/br/com/nataliabraz/orgs/MainActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/MainActivity.kt
@@ -7,8 +7,15 @@ import android.widget.TextView
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val view = TextView(this)
-        view.setText("Cesta de Frutas")
         setContentView(R.layout.activity_main)
+
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Salada de Frutas"
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, Morango e Banana"
+
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "20.00"
     }
 }
