d7f4d01924954335b09e6b435eb8a7d4cd154299
Natalia Braz <naabraz@gmail.com>
2023-12-07 09:42:46 -0300

    chore: use app compat on activity

--- app/src/main/java/br/com/nataliabraz/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,12 +1,6 @@
 package br.com.nataliabraz.orgs.ui.activity
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.nataliabraz.orgs.R
 
-class FormularioProdutoActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_formulario_produto)
-    }
-}
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto)
--- app/src/main/java/br/com/nataliabraz/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/MainActivity.kt
@@ -1,17 +1,16 @@
 package br.com.nataliabraz.orgs.ui.activity
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.nataliabraz.orgs.R
 import br.com.nataliabraz.orgs.model.Produto
 import br.com.nataliabraz.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
