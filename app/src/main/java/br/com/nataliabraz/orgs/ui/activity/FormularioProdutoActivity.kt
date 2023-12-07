486d8320c9e4f6b2b26af9edf19e1c05ebd2ebf2
Natalia Braz <naabraz@gmail.com>
2023-12-07 09:27:28 -0300

    chore: add new activity

---
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.nataliabraz.orgs.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.nataliabraz.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+}
