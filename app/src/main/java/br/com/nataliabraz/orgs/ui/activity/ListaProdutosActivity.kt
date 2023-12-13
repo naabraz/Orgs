0cbb9fe67b32528b233565bd961f67a9a8f23eac
Natalia Braz <naabraz@gmail.com>
2023-12-13 10:04:15 -0300

    chore: refactor code

---
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/ListaProdutosActivity.kt
@@ -0,0 +1,43 @@
+package br.com.nataliabraz.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import br.com.nataliabraz.orgs.R
+import br.com.nataliabraz.orgs.dao.ProdutosDAO
+import br.com.nataliabraz.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
+    private val dao = ProdutosDAO()
+    private val adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        configuraRecyclerView()
+        configuraFab()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.atualiza(dao.buscaTodos())
+    }
+
+    private fun configuraFab() {
+        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_produtos_fab)
+        fab.setOnClickListener {
+            vaiParaFormularioProduto()
+        }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.activity_lista_produtos_recyclerView)
+        recyclerView.adapter = adapter
+    }
+}
