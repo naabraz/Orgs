44c06648d106d8adf60149d298ee1a27b546aa6d
Natalia Braz <naabraz@gmail.com>
2024-02-15 09:48:53 -0300

    feat: receive extras from activity

--- app/src/main/java/br/com/nataliabraz/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -1,8 +1,10 @@
 package br.com.nataliabraz.orgs.ui.activity
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import br.com.nataliabraz.orgs.model.Produto
 
 class DetalhesProdutoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +12,15 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         title = "Detalhes produto"
 
         val intent = intent
-        val produto = intent.getBundleExtra("produto")
+        val produto = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra("produto", Produto::class.java)
+        } else {
+            intent.getParcelableExtra("produto")
+        }
 
-        Log.i("===DetalhesProdutoActivity", "onCreate: $produto")
+        Log.i("===Produto", "nome: ${produto?.nome}")
+        Log.i("===Produto", "descricao: ${produto?.descricao}")
+        Log.i("===Produto", "valor: ${produto?.valor}")
+        Log.i("===Produto", "imagem: ${produto?.imagem}")
     }
 }
