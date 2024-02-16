c736fc9a1e35d5d082b38d87743b224acdc19451
Natalia Braz <naabraz@gmail.com>
2024-02-16 10:57:24 -0300

    feat: hide action bar from detalhes produto screen

--- app/src/main/java/br/com/nataliabraz/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -4,12 +4,23 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import br.com.nataliabraz.orgs.databinding.ActivityDetalhesProdutoBinding
+import br.com.nataliabraz.orgs.extensions.carregar
 import br.com.nataliabraz.orgs.model.Produto
 
 class DetalhesProdutoActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityDetalhesProdutoBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "Detalhes produto"
+        setContentView(binding.root)
+
+//        if (supportActionBar != null) {
+//            supportActionBar!!.hide()
+//        }
 
         val intent = intent
         val produto = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -17,6 +28,8 @@ class DetalhesProdutoActivity : AppCompatActivity() {
         } else {
             intent.getParcelableExtra("produto")
         }
+
+        binding.activityDetalheProdutoImagem.carregar(this, produto?.imagem)
 
         Log.i("===Produto", "nome: ${produto?.nome}")
         Log.i("===Produto", "descricao: ${produto?.descricao}")
