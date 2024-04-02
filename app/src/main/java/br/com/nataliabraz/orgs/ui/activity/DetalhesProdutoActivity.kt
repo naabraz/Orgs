1eac85f3b07a39cb8c605c5bb222e815288052f3
Natalia Braz <naabraz@gmail.com>
2024-04-02 09:50:48 -0300

    feat: add product texts

--- app/src/main/java/br/com/nataliabraz/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -2,7 +2,6 @@ package br.com.nataliabraz.orgs.ui.activity
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import br.com.nataliabraz.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.nataliabraz.orgs.extensions.carregar
@@ -30,12 +29,10 @@ class DetalhesProdutoActivity : AppCompatActivity() {
             binding.activityDetalheProdutoImagem.carregar(this, produto.imagem)
 
             val valorEmMoeda = formataParaMoedaBrasileira(produto.valor)
-            binding.activityDetalheProdutoValor.text = valorEmMoeda;
 
-            Log.i("===Produto", "nome: ${produto.nome}")
-            Log.i("===Produto", "descricao: ${produto.descricao}")
-            Log.i("===Produto", "valor: ${produto.valor}")
-            Log.i("===Produto", "imagem: ${produto.imagem}")
+            binding.activityDetalheProdutoValor.text = valorEmMoeda;
+            binding.activityDetalheProdutoItemNome.text = produto.nome
+            binding.activityDetalheProdutoItemDescricao.text = produto.descricao
         }
     }
 }
