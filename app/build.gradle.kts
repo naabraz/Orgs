726fe295fe56b843d89638750917aa23b2bb9ad2
Natalia Braz <naabraz@gmail.com>
2024-04-11 10:01:48 -0300

    feat: adjust java 21 compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ android {
         viewBinding = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.11"
     }
     packaging {
         resources {
