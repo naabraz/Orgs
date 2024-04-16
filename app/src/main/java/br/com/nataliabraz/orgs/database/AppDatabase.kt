9de7e53bf1461567bb58e600e55af98ab3569dd7
Natalia Braz <naabraz@gmail.com>
2024-04-16 09:41:16 -0300

    chore: remove schema warning

--- app/src/main/java/br/com/nataliabraz/orgs/database/AppDatabase.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/database/AppDatabase.kt
@@ -9,7 +9,7 @@ import br.com.nataliabraz.orgs.database.converter.Converters
 import br.com.nataliabraz.orgs.database.dao.ProdutoDao
 import br.com.nataliabraz.orgs.model.Produto
 
-@Database(entities = [Produto::class], version = 1)
+@Database(entities = [Produto::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun produtoDao(): ProdutoDao
