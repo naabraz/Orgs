daa48098c557d0f3fbcd25d62a2364ac5a20eed4
Natalia Braz <naabraz@gmail.com>
2024-04-30 09:30:53 -0300

    feat: add order methods

--- app/src/main/java/br/com/nataliabraz/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/ListaProdutosActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.PopupMenu
+import android.widget.PopupMenu.OnMenuItemClickListener
 import androidx.appcompat.app.AppCompatActivity
 import br.com.nataliabraz.orgs.R
 import br.com.nataliabraz.orgs.database.AppDatabase
@@ -36,9 +39,73 @@ class ListaProdutosActivity : AppCompatActivity() {
         adapter.atualiza(produtoDao.buscaTodos())
     }
 
+    private fun ordenaPorNome(ordem: String) {
+        val db = AppDatabase.instancia(this)
+        val produtoDao = db.produtoDao()
+
+        when(ordem) {
+            "asc" -> adapter.atualiza(produtoDao.ordenaPorNomeAsc())
+            "desc" -> adapter.atualiza(produtoDao.ordenaPorNomeDesc())
+        }
+    }
+
+    private fun ordenaPorDescricao(ordem: String) {
+        val db = AppDatabase.instancia(this)
+        val produtoDao = db.produtoDao()
+
+        when(ordem) {
+            "asc" -> adapter.atualiza(produtoDao.ordenaPorDescricaoAsc())
+            "desc" -> adapter.atualiza(produtoDao.ordenaPorDescricaoDesc())
+        }
+    }
+
+    private fun ordenaPorValor(ordem: String) {
+        val db = AppDatabase.instancia(this)
+        val produtoDao = db.produtoDao()
+
+        when(ordem) {
+            "asc" -> adapter.atualiza(produtoDao.ordenaPorValorAsc())
+            "desc" -> adapter.atualiza(produtoDao.ordenaPorValorDesc())
+        }
+    }
+
+    private fun removeOrdenacao() {
+        val db = AppDatabase.instancia(this)
+        val produtoDao = db.produtoDao()
+
+        adapter.atualiza(produtoDao.buscaTodos())
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_ordena_lista, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_ordena_lista_filtrar_nome_asc -> {
+                ordenaPorNome("asc")
+            }
+            R.id.menu_ordena_lista_filtrar_nome_desc -> {
+                ordenaPorNome("desc")
+            }
+            R.id.menu_ordena_lista_filtrar_descricao_asc -> {
+                ordenaPorDescricao("asc")
+            }
+            R.id.menu_ordena_lista_filtrar_descricao_desc -> {
+                ordenaPorDescricao("desc")
+            }
+            R.id.menu_ordena_lista_filtrar_valor_asc -> {
+                ordenaPorValor("asc")
+            }
+            R.id.menu_ordena_lista_filtrar_valor_desc -> {
+                ordenaPorValor("desc")
+            }
+            R.id.menu_ordena_lista_filtrar_sem_ordenacao -> {
+                removeOrdenacao()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun configuraFab() {
