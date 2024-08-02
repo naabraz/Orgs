688685fff1823c32d86a18509324a1f026e52472
Natalia Braz <naabraz@gmail.com>
2024-08-02 11:05:10 -0300

    feat: add produtos to lista

--- app/src/main/java/br/com/nataliabraz/orgs/ui/activity/ListaTodosProdutosActivity.kt
+++ app/src/main/java/br/com/nataliabraz/orgs/ui/activity/ListaTodosProdutosActivity.kt
@@ -1,16 +1,53 @@
 package br.com.nataliabraz.orgs.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import br.com.nataliabraz.orgs.database.AppDatabase
 import br.com.nataliabraz.orgs.databinding.ActivityListaTodosProdutosBinding
+import br.com.nataliabraz.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import kotlinx.coroutines.launch
 
 class ListaTodosProdutosActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityListaTodosProdutosBinding.inflate(layoutInflater)
     }
 
+    private val adapter by lazy {
+        ListaProdutosAdapter(context = this)
+    }
+
+    private val produtoDao by lazy {
+        AppDatabase.instancia(this).produtoDao()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        configuraRecyclerView()
+
+        lifecycleScope.launch {
+            buscaTodosProdutos()
+        }
+    }
+
+    private suspend fun buscaTodosProdutos() {
+        produtoDao.buscaTodos().collect { produtos ->
+            adapter.atualiza(produtos)
+        }
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = binding.activityListaTodosProdutosRecyclerView
+        recyclerView.adapter = adapter
+
+        adapter.quandoClicaNoItem = {
+            val intent = Intent(this, DetalhesProdutoActivity::class.java)
+                .apply {
+                    putExtra(CHAVE_PRODUTO_ID, it.id)
+                }
+            startActivity(intent)
+        }
     }
 }
